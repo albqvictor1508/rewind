697fa9a6c281281a1b8272e75b9c6f28f9059862
Victor Albuquerque <albq.victor@gmail.com>
2025-10-27 19:27:21 -0300

    chore(movies): change select to get all movies info

--- api/src/modules/movies/index.ts
+++ api/src/modules/movies/index.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import { auth } from "src/config/auth";
 import { MovieService } from "./service";
 import { z } from "zod";
+import { db } from "src/db/client";
+import { movies } from "src/db/schema/movies";
+import { eq } from "drizzle-orm";
 
 export const movieRoutes = Router();
 
@@ -13,6 +16,7 @@ movieRoutes.get("/", async (_, response) => {
 movieRoutes.get("/user", auth.authenticate, async (request, response) => {
   // @ts-ignore
   const { id: userId } = request.user;
+
   const movies = await MovieService.getUserMovies(userId);
   return response.send(movies);
 });
@@ -21,6 +25,11 @@ movieRoutes.get("/:movieId", async (request, response) => {
   const { movieId } = z.object({
     movieId: z.uuid()
   }).parse(request.params);
+
+  const movieById = await db.query.movies.findFirst({
+    where: eq(movies.id, movieId)
+  });
+  if (!movieById) return response.status(400).json("Invalid movie.");
 
   const movie = await MovieService.getMovie(movieId);
   return response.send(movie);
