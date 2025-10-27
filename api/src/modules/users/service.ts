80fec2af599faaab635170cf2a1690c1bcb008cd
Victor Albuquerque <albq.victor@gmail.com>
2025-10-27 00:04:38 -0300

    chore(user): add user movies method

--- api/src/modules/users/service.ts
+++ api/src/modules/users/service.ts
@@ -1,7 +1,27 @@
+<<<<<<< Updated upstream
 import { randomBytes } from "node:crypto";
 
 export class UserService {
   public static genCode() {
     return randomBytes(3).toString('hex').toUpperCase()
+=======
+import { eq } from "drizzle-orm";
+import { db } from "src/db/client";
+import { movieMarks } from "src/db/schema/movieMarks";
+import { movies } from "src/db/schema/movies";
+
+export class UserService {
+  public static async getUserMovies(userId: string) {
+    const [userMovies] = await db.select({
+      title: movies.originalTitle,
+      photo: movies.photo,
+      average: movies.voteAverage
+    }).from(movies)
+      .innerJoin(movieMarks, eq(movies.id, movieMarks.movieId))
+      .where(eq(movieMarks.userId, userId))
+      .orderBy(movieMarks.createdAt)
+
+    return userMovies ?? [];
+>>>>>>> Stashed changes
   }
 }
