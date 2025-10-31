c85ae271acbbd42da0518a7a8526051d8b7e8d00
Victor Albuquerque <albq.victor@gmail.com>
2025-10-31 12:41:44 -0300

    wip(home): initial commit

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,9 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Outlet, createFileRoute } from '@tanstack/react-router'
+import { Menu } from '@/components/menu'
 
 export const Route = createFileRoute('/_app')({
-  component: RouteComponent,
+  component: AppLayout
 })
 
-function RouteComponent() {
-  return <div>Hello "/_app"!</div>
+function AppLayout() {
+  return (
+    <div className="w-screen h-full flex flex-col space-y-16">
+      <Menu />
+      <Outlet />
+    </div>
+  )
 }
--- web/src/pages/_app/home.tsx
+++ web/src/pages/_app/home.tsx
@@ -1,15 +1,101 @@
-import { createFileRoute, Link } from "@tanstack/react-router"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute('/_app/home')({
   component: RouteComponent
 })
 
+//TODO: terminar isso aqui
+
 export function RouteComponent() {
+  const categories = [
+    {
+      name: 'Ação',
+      movies: [
+        {
+          title: 'Batman',
+          photo: '/batman.jpg',
+          rate: 4.8,
+          marks: {
+            isFavorite: true,
+            status: 'WATCHED'
+          }
+        },
+        {
+          title: 'Homem Aranha',
+          photo: '/aranha.jpg',
+          rate: 4.7,
+          marks: {
+            isFavorite: false,
+            status: 'WATCHING'
+          }
+        },
+        {
+          title: 'Flash',
+          photo: '/flash.jpg',
+          rate: 4.5,
+          marks: {
+            isFavorite: false,
+            status: 'TO_WATCH'
+          }
+        }
+      ]
+    },
+    {
+      name: 'Vilões',
+      movies: [
+        {
+          title: 'Coringa',
+          photo: '/coringa.jpg',
+          rate: 4.9,
+          marks: {
+            isFavorite: true,
+            status: 'WATCHED'
+          }
+        },
+        {
+          title: 'Coringa 2',
+          photo: '/coringa-2.jpg',
+          rate: 5.0,
+          marks: {
+            isFavorite: true,
+            status: 'TO_WATCH'
+          }
+        },
+        {
+          title: 'Thanos',
+          photo: '/thanos.jpg',
+          rate: 4.8,
+          marks: {
+            isFavorite: false,
+            status: 'WATCHED'
+          }
+        },
+        {
+          title: 'Thanos 2',
+          photo: '/thanos-2.jpg',
+          rate: 4.7,
+          marks: {
+            isFavorite: false,
+            status: 'WATCHED'
+          }
+        }
+      ]
+    },
+    {
+      name: 'Comédia',
+      movies: []
+    },
+    {
+      name: 'Drama',
+      movies: []
+    }
+
+
+  ]
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div >
+    <div className="w-full h-full flex justify-center">
+      <h3>Mais Vistos da Semana</h3>
+    </div>
   )
 }
